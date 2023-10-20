faa89b0cba4b0a78b1cfd9ce709dfa8b01cb9d77
bssaranya <bssaranya2919@gmail.com>
2023-10-20 12:47:11 +0530

    added test

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+#Calculator Function
+
+
+def add(x, y):
+    #Add x and y and return the result
+    return(x + y)
+
+def subtract(x, y):
+    #Subtract x from y and return the result
+    return(x - y)
